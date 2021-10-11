547b219c93644f9b28b96a92306cc648458308f6
michael-grace <michael.dp.grace@outlook.com>
2021-10-11 12:57:04 +0100

    submit study data back to server

--- frontend/pages/studies/[id].jsx
+++ frontend/pages/studies/[id].jsx
@@ -1,20 +1,38 @@
 import {useRouter} from "next/router"
 import { useEffect, useState } from "react";
-import { apiRequest, keyCheck } from "../../utils/api"
+import { apiRequest, keyCheck, postApiReqiest } from "../../utils/api"
 import styles from '../../styles/Home.module.css'
 
 const Study = () => {
     const router = useRouter();
 
+    const [studyId, setStudyId] = useState("");
     const [studyData, setStudyData] = useState(Object);
+    const [newData, setNewData] = useState(Object);
 
     useEffect(() => {
         var {id} = router.query;
         if (id) {
             keyCheck()
-            apiRequest(`studies/${id}`).then((study) => {if (study) {setStudyData({...study.data})}});
+            setStudyId(id);
+            apiRequest(`studies/${id}`).then((study) => {
+                if (study) {
+                    setStudyData({...study.data});
+                    setNewData({...study.data});
+                }
+            });
         }
     }, [router.query])
+
+    const submitForm = async () => {
+        // TODO Actual feedback to user, not just console logs
+        var req = await postApiReqiest(`studies/${studyId}`, newData)
+        if (req) {
+            console.log("OK")
+        } else {
+            console.log("Not OK")
+        }
+    }
 
     return (
         <div className={styles.main}>
@@ -23,10 +41,17 @@ const Study = () => {
                 {Object.keys(studyData).map((key) => (
                     <div key={key} className="form-group">
                         <label htmlFor={key}>{key}</label>
-                        <input type="text" className="form-control" id={key} defaultValue={studyData[key]}></input>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            id={key} 
+                            defaultValue={studyData[key]} 
+                            onChange={e => {setNewData({...newData, [key]: e.target.value})}} 
+                            />
                         <br />
                     </div>
                 ))}
+                <button type="button" className="btn btn-primary" onClick={submitForm}>Submit</button>
             </form>
         </div>
     )
--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -7,6 +7,17 @@ export const apiRequest = async (endpoint) => {
     return await r.json()
   }
 
+export const postApiReqiest = async (endpoint, body) => {
+    const r = await fetch(`/api/${endpoint}`, {
+        method: "POST",
+        headers: {
+            "Genestack-API-Token": localStorage.getItem("Genestack-API-Token")
+        },
+        body: JSON.stringify(body)
+    })
+    return r.ok
+}
+
 export const keyCheck = () => {
     if (localStorage.getItem("Genestack-API-Token") == null) {
         window.location = "/"
