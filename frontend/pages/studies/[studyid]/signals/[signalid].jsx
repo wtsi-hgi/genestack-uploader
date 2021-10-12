401a9c5fb88803f60550ac3d3942b516025fb565
michael-grace <michael.dp.grace@outlook.com>
2021-10-12 10:24:56 +0100

    signal metadata in form

--- frontend/pages/studies/[studyid]/signals/[signalid].jsx
+++ frontend/pages/studies/[studyid]/signals/[signalid].jsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react";
-import { apiRequest, keyCheck } from "../../../../utils/api";
+import { apiRequest, keyCheck, postApiReqiest } from "../../../../utils/api";
 import styles from '../../../../styles/Home.module.css'
 
 const Signal = () => {
@@ -19,16 +19,41 @@ const Signal = () => {
             setSignalId(signalid);
             apiRequest(`studies/${studyid}/signals/${signalid}`).then((signal) => {
                 if (signal) {
-                    setSignalData({...signal.data});
+                    setSignalData({...signal.data.signal.metadata});
                     setNewData({...signal.data});
                 }
             })
         }
     }, [router.query])
 
+    const submitForm = async () => {
+        // TODO Actual User Feedback
+        var req = await postApiReqiest(`studies/${studyId}/signals/${signalId}`, newData)
+        if (req) {
+            console.log("OK")
+        } else {
+            console.log("Not OK")
+        }
+    }
+
     return (
         <div className={styles.main}>
-            <p>{JSON.stringify(signalData)}</p>
+            <form>
+                {Object.keys(signalData).map((key) => (
+                    <div key={key} className="form-group">
+                        <label htmlFor={key}>{key}</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name={key}
+                            defaultValue={signalData[key]}
+                            onChange={e => {setNewData({...newData, [key]: e.target.value})}}
+                        />
+                        <br />
+                    </div>
+                ))}
+                <button type="button" className="btn btn-primary" onClick={submitForm}>Submit</button>
+            </form>
         </div>
     )
 }
--- frontend/pages/studies/[studyid]/signals/index.jsx
+++ frontend/pages/studies/[studyid]/signals/index.jsx
@@ -1,5 +1,102 @@
-const Hello = () => {
-    return (<h1>Hello</h1>)
+import { useEffect, useState } from "react"
+import { apiRequest, keyCheck, postApiReqiest } from "../../../../utils/api";
+import styles from "../../../../styles/Home.module.css"
+import { useRouter } from "next/router";
+
+
+const NewSignal = () => {
+    const router = useRouter();
+
+    const [studyId, setStudyId] = useState("");
+    const [templates, setTemplates] = useState([]);
+    const [selectedTemplate, setSelectedTemplate] = useState("");
+    const [fullTemplateFields, setFullTemplateFields] = useState([]);
+    const [templateSubtypes, setTemplateSubtypes] = useState([]);
+    const [selectedTemplateSubtype, setSelectedTemplateSubtype] = useState("");
+    const [templateFields, setTemplateFields] = useState([]);
+    const [newSignal, setNewSignal] = useState(Object);
+
+    useEffect(() => {
+        var {studyid} = router.query;
+
+        if (studyid) {
+            keyCheck()
+            setStudyId(studyid)
+            apiRequest("templates").then((t) => {
+                setTemplates(t.data.map(e => ({"name": e.name, "accession": e.accession})))
+            })
+        }
+    }, [router.query])
+
+    const loadTemplate = () => {
+        apiRequest(`templates/${selectedTemplate}`).then(t => {
+            setFullTemplateFields(t.data.template)
+            setTemplateSubtypes([...new Set(t.data.template.map(e => e.dataType))]);
+        })
+    }
+
+    const loadTemplateSubtype = () => {
+        var fields = fullTemplateFields.filter(e => !e.isReadOnly && e.dataType == selectedTemplateSubtype).map(e => e.name)
+        setTemplateFields(fields)
+        setNewSignal(fields.reduce((xs, x) => ({...xs, [x]: ""}), {}))
+    }
+
+    const submitSignal = async () => {
+        // TODO Actual Feedback
+        var req = await postApiReqiest(`studies/${studyId}/signals`, newSignal)
+        if (req) {
+            console.log("OK")
+        } else {
+            console.log("Not OK")
+        }
+    }
+
+    return (
+        <div className={styles.main}>
+            <h1>New Signal</h1>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="select-template">Select a Template</label>
+                    <select className="form-select" name="select-template"
+                        onChange={e => {setSelectedTemplate(e.target.value)}}>
+                        {templates.map(e => (<option key={e.accession} value={e.accession}>{e.name}</option>))}
+                    </select>
+                    <br />
+                    <button type="button" className="btn btn-primary"
+                        onClick={loadTemplate}>Load Template</button>
+                </div>
+            </form>
+            <br />
+            <form>
+                {templateSubtypes.length != 0 && <div className="form-group">
+                    <label htmlFor="select-template-subtype">Select Template Subtype</label>
+                    <select className="form-select" name="select-template-subtype"
+                        onChange={e => {setSelectedTemplateSubtype(e.target.value)}}>
+                        {templateSubtypes.map(e => (<option key={`subtype-${e}`} value={e}>{e}</option>))}
+                    </select>
+                    <br />
+                    <button type="button" className="btn btn-primary"
+                        onClick={loadTemplateSubtype}>Load Template Subtype</button>
+                </div>}
+            </form>
+            <br />
+            <form>
+                {templateFields.map(e => (
+                    <div key={e} className="form-group">
+                        <label htmlFor={e}>{e}</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name={e}
+                            onChange={event => {setNewSignal({...newSignal, [e]: event.target.value})}}
+                        />
+                        <br />
+                    </div>
+                ))}
+                {templateFields.length != 0 && (<button type="button" className="btn btn-primary" onClick={submitSignal}>Submit</button>)}
+            </form>
+        </div>
+    )
 }
 
-export default Hello;
+export default NewSignal;
