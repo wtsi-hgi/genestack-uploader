bd92c1ba8d523fa260c375de2b0f9dd0a8e78c46
michael-grace <michael.dp.grace@outlook.com>
2021-11-09 13:26:04 +0000

    license docs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,28 @@
-var host = process.env.NEXT_PUBLIC_HOST.split("/")
+/**
+Genestack Uploader
+A HTTP server providing an API and a frontend for easy uploading to Genestack
+
+Copyright (C) 2021 Genome Research Limited
+
+Author: Michael Grace <mg38@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+var host = process.env.NEXT_PUBLIC_HOST.split("/");
 
 module.exports = {
   reactStrictMode: false,
   basePath: "/" + host[host.length - 1]
-}
+};
